7cacdbc93e36ad7d5b96d664db04450e89a2f29f
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-27 00:36:50 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,6 +7,6 @@ i = i + 1
 end
 end
 
-hello_t(["Tim", "Tom", "Jim"]) {|name| if name.start_with("T")
-puts "Hi, #{name}"}
+hello_t(["Tim", "Tom", "Jim"]) {|name| if name.start_with("T")}
+puts "Hi, #{name}"
 end
